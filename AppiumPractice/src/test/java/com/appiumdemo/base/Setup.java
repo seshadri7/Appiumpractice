145030509370fb6761a42419c2dd58ddce8b04d5
sesh <sesh@seshadri>
2018-12-30 22:53:29 +0530

    First commit

---
+++ AppiumPractice/src/test/java/com/appiumdemo/base/Setup.java
@@ -0,0 +1,52 @@
+package com.appiumdemo.base;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
+
+public class Setup {
+	
+	public static AndroidDriver driver;
+	
+	@Test
+	public static void applauch() throws MalformedURLException {
+		
+		AppiumDriverLocalService service = AppiumDriverLocalService.buildService(
+				new AppiumServiceBuilder().usingDriverExecutable(new File("C:\\Program Files\\nodejs\\node.exe"))
+				.withAppiumJS(new File("C:\\Users\\sesh\\AppData\\Local\\Programs\\Appium\\resources\\app\\node_modules\\appium\\build\\lib\\main.js"))
+				.withLogFile(new File(System.getProperty("user.dir")+"\\src\\test\\resources\\com\\appiumdemo\\logs\\appium.logs")).withArgument(GeneralServerFlag.LOCAL_TIMEZONE));
+		
+		service.start();
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "48fedac1");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1.0");
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+		cap.setCapability("appPackage", "com.oneplus.camera");
+		cap.setCapability("appActivity", "com.oneplus.camera.OPCameraActivity");
+		//cap.setCapability(MobileCapabilityType.APPLICATION_NAME, " ");
+		
+		driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"),cap);
+		driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+		
+		
+		service.stop();
+		
+		
+	}
+	
+	
+
+}
